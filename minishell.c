223a80b93c6f3d991545be5979b6f710992df51c
vipereir <zzvtrx@gmail.com>
2022-12-05 11:27:57 -0300

    executando o comando em um fork

--- minishell.c
+++ minishell.c
@@ -2,16 +2,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int	main(void)
+int	main(int argc, char *argv[], char *envp[])
 {
+	pid_t	c_pid;
+
+	if (argc != 1)
+		return (0);
 	char	*command;
 	while (1)
 	{
 		command = readline("minish> ");
+		c_pid = fork();
+		if (c_pid == 0)
+		{
+			execve(command, argv, envp);
+			return (0);
+		}
+		else
+			wait(NULL);
+
 		free(command);
 	}
 	return (0);

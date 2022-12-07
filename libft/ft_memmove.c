1a9025d8bc8ffc3f6f60a174f1776b581a7755de
sxpphic <sphhhsh@gmail.com>
2022-12-07 05:41:55 -0300

    ft_memmove mod

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -23,7 +23,8 @@ void	*ft_memmove(void	*dst, const void	*src, size_t	n)
 	else
 	{
 	i = n - 1;
-		while (i >= 0 && i < n)
+		//while (i >= 0 && i < n)
+		while (i > 0 && i < n)
 		{
 			((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
 			i--;
